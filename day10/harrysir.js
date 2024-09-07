f89c21ff6be2e971c08dd195654bbf28238e76c0
Aashish Prajapat <aashishprajapat454@gmail.com>
2024-09-07 19:36:04 +0530

    save

---
+++ day10/harrysir.js
@@ -0,0 +1,15 @@
+// console.log('hyy\n mr');
+// console.log('hyy\t mr');
+// console.log('h\ryy mr');
+
+let myArr = [12,23,34,45,56,67,78,89,90,1]
+let myNewArr = myArr.splice(-3,11)
+// console.log(myNewArr);
+// console.log(myArr);
+
+
+// *****************compaireson*********************
+// 0 to other
+console.log(null === 0);
+console.log(null === -0);
+console.log(null === undefined);
