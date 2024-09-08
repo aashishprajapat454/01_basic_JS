de37fd957e572d4791ada9d80584a9ce3c1e3121
Aashish Prajapat <aashishprajapat454@gmail.com>
2024-09-08 13:29:14 +0530

    yes

---
+++ day11/arr.js
@@ -0,0 +1,9 @@
+let arr = [12,23,34,45,56,67,78,89,90];
+let Narr = []
+for(let i = arr.length; i>=0; i--){
+    Narr.push(arr[i])
+}
+console.log(Narr);
+// console.log(arr.length);
+let hap = (arr.length)/2
+console.log(hap);
